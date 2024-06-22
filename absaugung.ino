496b67bfaea2b7c00a095b3f7b3ac0069aacb1b7
Frank Prumbaum <fprumbau@gmail.com>
2024-06-22 20:16:09 +0200

    Update absaugung.ino

--- absaugung.ino
+++ absaugung.ino
@@ -8,6 +8,10 @@
     int bg2 = 4;
     int bg3 = 7;
 
+    bool bg1set = false;
+    bool bg2set = false;
+    bool bg3set = false;
+
     int Grad;
 
     //___________________VOID SETUP______________
@@ -37,45 +41,53 @@
   //___________FUNKTION "BlastGate1"___________________
 
     void BG1(int Stellung) {              //0=zu  1=auf
-      if(Stellung == 0) {                 //Blast Gate 1 zu
+      if(bg1set && Stellung == 0) {
         Grad = 0;                         //Position in Grad
         digitalWrite(8, LOW);             //Signalleuchte aus
+        schalten(blastgate1, 11, Grad);    //(Servo, Anschlusspin, Position)
+        bg1set = false;
       }
-      if(Stellung == 1) {                 //Blast Gate oeffnen
-        Grad = 360;                       //Position in Grad
-        digitalWrite(8, HIGH);            //Signalleuchte an
+      if(!bg1set && Stellung == 1) {
+        Grad = 180;                         //Position in Grad
+        digitalWrite(8, HIGH);             //Signalleuchte aus
+        schalten(blastgate1, 11, Grad);    //(Servo, Anschlusspin, Position)
+        bg1set = true;
       }
-      schalten(blastgate1, 11, Grad);      //(Servo, Anschlusspin, Position)
     }
 
   //___________FUNKTION "BlastGate2"___________________
 
     void BG2(int Stellung) {              //0=zu  1=auf
-      if(Stellung == 0) {                 //Blast Gate 1 zu
+      if(bg2set && Stellung == 0) {
         Grad = 0;                         //Position in Grad
-         digitalWrite(12, LOW);           //Signalleuchte aus
+        digitalWrite(12, LOW);             //Signalleuchte aus
+        schalten(blastgate2, 10, Grad);    //(Servo, Anschlusspin, Position)
+        bg2set = false;
       }
-      if(Stellung == 1) {                 //Blast Gate oeffnen
-        Grad = 180;                       //Position in Grad
-        digitalWrite(12, HIGH);           //Signalleuchte an
-      }
-      schalten(blastgate2, 10, Grad);     //(Servo, Anschlusspin, Position)
+      if(!bg2set && Stellung == 1) {
+        Grad = 180;                         //Position in Grad
+        digitalWrite(12, HIGH);             //Signalleuchte aus
+        schalten(blastgate2, 10, Grad);    //(Servo, Anschlusspin, Position)
+        bg2set = true;
+      }    
     }
 
   //___________FUNKTION "BlastGate3"___________________
 
     void BG3(int Stellung) {              //0=zu  1=auf
-      if(Stellung == 0) {                 //Blast Gate 1 zu
+      if(bg3set && Stellung == 0) {
         Grad = 0;                         //Position in Grad
-        digitalWrite(13, LOW);            //Signalleuchte aus
+        digitalWrite(13, LOW);             //Signalleuchte aus
+        schalten(blastgate3, 9, Grad);    //(Servo, Anschlusspin, Position)
+        bg3set = false;
       }
-      if(Stellung == 1) {                 //Blast Gate oeffnen
-        Grad = 180;                       //Position in Grad
-        digitalWrite(13, HIGH);           //Signalleuchte an
+      if(!bg3set && Stellung == 1) {
+        Grad = 180;                         //Position in Grad
+        digitalWrite(13, HIGH);             //Signalleuchte aus
+        schalten(blastgate3, 9, Grad);    //(Servo, Anschlusspin, Position)
+        bg3set = true;
       }
-      schalten(blastgate3, 9, Grad);     //(Servo, Anschlusspin, Position)
   }
-
 
 
   //_____________VOID LOOP______________________
@@ -95,4 +107,4 @@ void loop() {
       BG1(0);
       BG2(0);
  }
-   }
+}
